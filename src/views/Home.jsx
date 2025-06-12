f465dc82920cd1171655ed2607a661609d789937
Nikita Pizarro <nikitta6928@gmail.com>
2025-06-12 20:48:59 -0300

    xx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -24,7 +24,35 @@ function Home() {
  <h1>Bienvenidos a la tienda</h1>
  <h2>Los mejores precios de la ciudad</h2>
   </section>
+
+  <section className="productsList">
+   <div>
+    <h2>Bicicleta</h2>
+    <p>Precio:1000</p>
+    <p>20 cambios, cuadro de aluminio,disponible en todos los rodados</p>
+    <button>Comprar</button>
+   </div>
+
+   <div>
+    <h2>Computadora</h2>
+    <p>Precio:3000</p>
+    <p>Windows 11 instalado, paquete office con licencia, 16 de menoria Ram</p>
+    <button>Comprar</button>
+   </div>
+
+<div>
+    <h2>Silla Gamer</h2>
+    <p>Precio:2000</p>
+    <p>Tapizado de eco-cuerina, altura regulable, fácil de limpiar</p>
+    <button>Comprar</button>
+   </div>
+
+  </section>
  </main>
+
+ <footer>
+  <p>Sitio creado por Nikita Pizarro Cortés</p>
+ </footer>
 
 </>
   )
