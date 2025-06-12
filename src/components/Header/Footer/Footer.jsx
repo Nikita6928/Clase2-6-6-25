857ec031b5ccf7e83695eeb76e557d4e9d3fc67c
Nikita Pizarro <nikitta6928@gmail.com>
2025-06-12 11:57:26 -0300

    Creando componentes

--- src/components/Header/Footer/Footer.jsx
+++ src/components/Header/Footer/Footer.jsx
@@ -1,1 +1,11 @@
 import ' ./Footer.css'
+
+const Footer=()=>{
+    return(
+   <footer>
+    <p>Sitio desarrollado por Nikita Pizarro Cortés</p>
+   </footer>
+    )
+}
+
+export default Footer
--- src/components/Header/Main/Main.jsx
+++ src/components/Header/Main/Main.jsx
@@ -1,1 +1,38 @@
  import'./Main.css'
+
+ const Main =()=>{
+    return (
+
+     <main>
+    <section className="banner">
+    <h1>Bienvenidos a la tienda</h1>
+    <h2>Los mejores precios de la ciudad</h2>
+    </section>
+    <section className="productList">
+<div className="products">
+  <h2>Bicicleta</h2>
+  <p>Precio: 1000</p>
+  <p>20 cambios, cuadro de aluminio,disponible en todos los rodados</p>
+  <button>Comprar</button>
+</div>
+
+<div className="products">
+  <h2>Computadora</h2>
+  <p>Precio: 3000</p>
+  <p>Windows 11 instalado, paquete office con licencia,16 de memoria ram </p>
+  <button>Comprar</button>
+</div>
+
+<div className="products">
+  <h2>Silla gamer</h2>
+  <p>Precio: 2000</p>
+  <p>Tapizado de eco-cuerina, altura regulable, fácil de limpiar</p>
+  <button>Comprar</button>
+</div>
+    </section>
+   </main>
+
+    )
+ }
+
+ export default Main
