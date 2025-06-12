719f354b0d9742bd4a45b514876483ffb0353c88
Nikita Pizarro <nikitta6928@gmail.com>
2025-06-12 20:26:42 -0300

    No sé si se solucinó

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 
 
 import "../styles/Home.css"
-import Header from '../components/Header/Header.jsx'
+/*import Header from '../components/Header/Header.jsx'*/
 
 
 function Home() {
@@ -9,15 +9,25 @@ function Home() {
 
   return (
     <>
-  {/* renderizar el Header*/}
-  <Header />
-  {/*renderizar el Main*/}
-  <Main />
-  {/*renderizar el Footer*/}
+ <header>
+  <nav>
+    <ul>
+      <li><a href="#"></a>Inicio</li>
+      <li><a href="/admin">Panel del administrador</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+  </nav>
+ </header>
 
+ <main>
+  <section className="banner">
+ <h1>Bienvenidos a la tienda</h1>
+ <h2>Los mejores precios de la ciudad</h2>
+  </section>
+ </main>
 
 </>
-  );
+  )
 }
 
 export default Home
