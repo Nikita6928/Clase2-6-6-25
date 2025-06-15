c5e19ec16b0fc3b16d6d4f5f4fed5375ae01b55a
Nikita Pizarro <nikitta6928@gmail.com>
2025-06-15 10:46:44 -0300

    solucionado

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,65 +1,65 @@
 import './Main.css'
 
-const Main =()=> {
-    //Simulando una lista de productos recibida desde una API//
-    //Ahora voy a crear el array//
+const Main = () => {
+   //Simulando una lista de productos recibida desde una API//
+   //Ahora voy a crear el array//
 
-    const products[ 
-       {
-        nombre:Bicicleta,
-        precio:1000,
-        descripcion: 20 cambios,cuadro de alumninio, disponible en todos los rodados,
-       },
+   const products = [
+      {
+         nombre: "Bicicleta",
+         precio: 1000,
+         descripcion: " 20 cambios,cuadro de alumninio, disponible en todos los rodados",
+      },
 
-       {
-        nombre:Computadora
-        precio:3000,
-        descripcion:Windows 11 instalado, paquete office con licencia, 16 de menoria Ram
-       },
+      {
+         nombre: "Computadora",
+         precio: 3000,
+         descripcion: "Windows 11 instalado, paquete office con licencia, 16 de menoria Ram",
+      },
 
-       {
-        nombre:Silla gamer
-        precio:2000,
-        descripcion:Tapizado de eco-cuerina, altura regulable, fácil de limpiar
-       }
+      {
+         nombre: "Silla gamer",
+         precio: 2000,
+         descripcion: "Tapizado de eco-cuerina, altura regulable, fácil de limpiar",
+      }
 
-       
-    ];                                                  
 
-    return(
-     <main>
-  <section className="banner">
- <h1>Bienvenidos a la tienda</h1>
- <h2>Los mejores precios de la ciudad</h2>
-  </section>
+   ];
 
-  <section className="productsList">
-    
-   <div className="products">
-    <h2>Bicicleta</h2>
-    <p>Precio:1000</p>
-    <p>20 cambios, cuadro de aluminio,disponible en todos los rodados</p>
-    <button>Comprar</button>
-   </div>
+   return (
+      <main>
+         <section className="banner">
+            <h1>Bienvenidos a la tienda</h1>
+            <h2>Los mejores precios de la ciudad</h2>
+         </section>
 
-   <div className="products">
-    <h2>Computadora</h2>
-    <p>Precio:3000</p>
-    <p>Windows 11 instalado, paquete office con licencia, 16 de menoria Ram</p>
-    <button>Comprar</button>
-   </div>
+         <section className="productsList">
 
-<div className="products">
-    <h2>Silla Gamer</h2>
-    <p>Precio:2000</p>
-    <p>Tapizado de eco-cuerina, altura regulable, fácil de limpiar</p>
-    <button>Comprar</button>
-   </div>
+            <div className="products">
+               <h2>Bicicleta</h2>
+               <p>Precio:1000</p>
+               <p>20 cambios, cuadro de aluminio,disponible en todos los rodados</p>
+               <button>Comprar</button>
+            </div>
 
-  </section>
- </main>
+            <div className="products">
+               <h2>Computadora</h2>
+               <p>Precio:3000</p>
+               <p>Windows 11 instalado, paquete office con licencia, 16 de menoria Ram</p>
+               <button>Comprar</button>
+            </div>
 
-    )
+            <div className="products">
+               <h2>Silla Gamer</h2>
+               <p>Precio:2000</p>
+               <p>Tapizado de eco-cuerina, altura regulable, fácil de limpiar</p>
+               <button>Comprar</button>
+            </div>
+
+         </section>
+      </main>
+
+   )
 
 }
 
