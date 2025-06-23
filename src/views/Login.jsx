58d4a7b927cd5d6ed0a36aa9639b327ead3d1727
Nikita Pizarro <nikitta6928@gmail.com>
2025-06-23 19:26:32 -0300

    practicando

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,39 +2,44 @@ import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 import Layout from "../components/Layout/Layout"
 import "../styles/Login.css"
+import { useState } from "react"
 
 
 const Login = () => {
-    return (
-        /*
-            <Layout>
-                <h1>Login</h1>
-                <form>
-                    <label htmlFor="name="></label>
-                    <input type="text" name="name" placeholder="Ingresá tu nombre de usuario" />
-                    <button>Ingresar</button>
-                </form>
-            </Layout>
 
-        </>*/
-
-
-
-        <Layout>
-            <section id="login-section">
-                <h1>Login</h1>
-                <form>
-                    <label htmlFor="email">Correo electrónico</label>
-                    <input type="email" name="email" id="email" />
-
-                    <label htmlFor="password">Contraseña:</label>
-                    <input type="password" name="password" id="passwoord" />
-                    <button>Ingresar</button>
-                </form>
-
-            </section>
-        </Layout>
-
-    )
 }
+
+
+return (
+    /*
+        <Layout>
+            <h1>Login</h1>
+            <form>
+                <label htmlFor="name="></label>
+                <input type="text" name="name" placeholder="Ingresá tu nombre de usuario" />
+                <button>Ingresar</button>
+            </form>
+        </Layout>
+ 
+    </>*/
+
+
+
+    <Layout>
+        <section id="login-section">
+            <h1>Login</h1>
+            <form>
+                <label htmlFor="email">Correo electrónico</label>
+                <input type="email" name="email" id="email" />
+
+                <label htmlFor="password">Contraseña:</label>
+                <input type="password" name="password" id="passwoord" />
+                <button>Ingresar</button>
+            </form>
+
+        </section>
+    </Layout>
+}
+)
+
 export default Login
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -3,28 +3,46 @@ EL fragmen, tampoco es nedesario, porque tengo el elemento padre, el Layout.
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"*/
 
+import { useState } from "react"
 import Layout from "../components/Layout/Layout"
 import "../styles/Register.css"
 
 
 const Register = () => {
-    return (
 
-        <Layout>
-            <section id="register-section">
-                <h1>Regístrate</h1>
-                <form>
-                    <label htmlFor="email">Correo electrónico</label>
-                    <input type="email" name="email" id="email" />
+    const [name, setName] = useState("")
+    const [email, setEMail] = useState("")
+    const [passwoord, setPassword] = useState("")
 
-                    <label htmlFor="password">Contraseña:</label>
-                    <input type="password" name="password" id="passwoord" />
-                    <button>Registrarse</button>
-                </form>
+    //El controlado del evento name
+    const handleName = (event) => {
+        setName(event.target.value)
+    }
+    //El manejador del correo
+    const handEmail = (event) => {
+        setEMail(event.target.value)
+    }
+    //El manejador de la contraseña
+    const handlePassword = (event) => {
+        setPassword(event.target.value)
+    }
+    <Layout>
+        <section id="register-section">
+            <h1>Regístrate</h1>
+            <form>
+                <label htmlFor="name">Ingresa tu nombre</label>
+                <input type="text" name="name" id="name" onChange={handleName} />
+                <label htmlFor="email">Correo electrónico</label>
+                <input type="email" name="email" id="email" onChange={handEmail} />
 
-            </section>
-        </Layout>
+                <label htmlFor="password">Contraseña:</label>
+                <input type="password" name="password" id="passwoord" onChange={handlePassword} />
+                <button>Registrarse</button>
+            </form>
 
-    )
+        </section>
+    </Layout>
+
+
 }
 export default Register
